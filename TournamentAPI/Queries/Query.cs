eab8c5e605a4c3d5b77bc75922a489c243f0d253
Dejmenek <dejmen14567@gmail.com>
2025-12-17 17:01:23 +0100

    refactor: separate query functionalities into classes
    Refactored the code to enhance modularity by creating distinct classes for query functionalities: `MatchQueries`, `TournamentQueries`, and `UserQueries`. The previous `Query` class has been removed, and its methods have been redistributed among the new classes.
    
    - `MatchQueries` now includes `GetMatchesForRound` for retrieving matches by round.
    - `TournamentQueries` provides methods for fetching non-deleted tournaments and retrieving a tournament by ID asynchronously.
    - `UserQueries` introduces `GetMe` for authenticated users to access their information.
    
    This restructuring improves code organization and clarity.

---
+++ TournamentAPI/Queries/Query.cs
@@ -0,0 +1,5 @@
+namespace TournamentAPI.Queries;
+
+public class Query
+{
+}
---
+++ TournamentAPI/Queries/UserQueries.cs
@@ -0,0 +1,21 @@
+using HotChocolate.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using TournamentAPI.Data;
+using TournamentAPI.Models;
+
+namespace TournamentAPI.Queries;
+
+[ExtendObjectType(typeof(Query))]
+public class UserQueries
+{
+    [Authorize]
+    public async Task<ApplicationUser> GetMe(ClaimsPrincipal claimsPrincipal, [Service] ApplicationDbContext context)
+    {
+        var userIdClaim = (claimsPrincipal?.FindFirstValue(ClaimTypes.NameIdentifier))
+            ?? throw new GraphQLException("User is not authenticated.");
+        var userId = int.Parse(userIdClaim);
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == userId);
+        return user ?? throw new GraphQLException("User not found.");
+    }
+}
