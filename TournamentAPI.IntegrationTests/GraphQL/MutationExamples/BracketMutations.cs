8914e1b4a79b6790a0170ad56a5697910ab86dd0
Dejmenek <dejmen14567@gmail.com>
2025-12-26 21:13:16 +0100

    test: add GraphQL bracket mutation examples
    Added TournamentAPI.IntegrationTests.GraphQL.MutationExamples namespace with a static Mutations class. Introduced Bracket class containing constant GraphQL mutation strings for generating brackets, generating brackets with matches, and updating rounds. These provide reusable mutation examples and error handling fragments for integration tests.

---
+++ TournamentAPI.IntegrationTests/GraphQL/MutationExamples/BracketMutations.cs
@@ -0,0 +1,96 @@
+namespace TournamentAPI.IntegrationTests.GraphQL.MutationExamples;
+public static partial class Mutations
+{
+    public static class Bracket
+    {
+        public const string GenerateBracket = """
+            mutation GenerateBracket($input: GenerateBracketInput!) {
+              generateBracket(input: $input) {
+                bracket {
+                  id
+                  tournamentId
+                }
+                errors {
+                  ... on BracketAlreadyExistsError {
+                    message
+                  }
+                  ... on BracketGenerationNotAllowedError {
+                    message
+                  }
+                  ... on NotEnoughParticipantsError {
+                    message
+                  }
+                  ... on TournamentNotFoundError {
+                    message
+                  }
+                  ... on TournamentNotOwnerError {
+                    message
+                  }
+                }
+              }
+            }
+            """;
+
+        public const string GenerateBracketWithMatches = """
+            mutation GenerateBracketWithMatches($input: GenerateBracketInput!) {
+              generateBracket(input: $input) {
+                bracket {
+                  id
+                  tournamentId
+                  matches {
+                    id
+                    round
+                    participantAId
+                    participantBId
+                    scoreA
+                    scoreB
+                    winnerId
+                  }
+                }
+                errors {
+                  ... on BracketAlreadyExistsError {
+                    message
+                  }
+                  ... on BracketGenerationNotAllowedError {
+                    message
+                  }
+                  ... on NotEnoughParticipantsError {
+                    message
+                  }
+                  ... on TournamentNotFoundError {
+                    message
+                  }
+                  ... on TournamentNotOwnerError {
+                    message
+                  }
+                }
+              }
+            }
+            """;
+
+        public const string UpdateRound = """
+            mutation UpdateRound($input: UpdateRoundInput!) {
+              updateRound(input: $input) {
+                bracket {
+                  id
+                  tournamentId
+                }
+                errors {
+                  ... on BracketNotFoundError {
+                    message
+                  }
+                  ... on NoMatchesInRoundError {
+                    message
+                  }
+                  ... on NotAllMatchesPlayedError {
+                    message
+                  }
+                  ... on TournamentNotOwnerError {
+                    message
+                  }
+                }
+              }
+            }
+            """;
+    }
+}
