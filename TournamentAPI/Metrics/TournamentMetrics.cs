6bb9eff249d81da2fd94688dda87524a44a69a0c
Dejmenek <dejmen14567@gmail.com>
2026-05-08 22:12:13 +0200

    feat: add TournamentMetrics for tournament creation
    Introduced TournamentMetrics class in TournamentAPI.Metrics using System.Diagnostics.Metrics. Added a counter to track the number of tournaments created, initialized via IMeterFactory. Provided a method to increment the counter, enabling instrumentation for tournament creation monitoring.

---
+++ TournamentAPI/Metrics/TournamentMetrics.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics.Metrics;
+
+namespace TournamentAPI.Metrics;
+
+public class TournamentMetrics
+{
+    private readonly Counter<int> _tournamentsCreated;
+
+    public TournamentMetrics(IMeterFactory meterFactory)
+    {
+        var meter = meterFactory.Create(MetricConstants.TournamentMeterName);
+        _tournamentsCreated = meter.CreateCounter<int>("tournamentapi.tournaments.tournaments_created", description: "Number of tournaments created");
+    }
+
+    public void IncrementTournamentsCreated()
+    {
+        _tournamentsCreated.Add(1);
+    }
+}
