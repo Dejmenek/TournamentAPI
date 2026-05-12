e63b9428c1811caa21be08adc8e0a3ed77d7d111
Dejmenek <dejmen14567@gmail.com>
2026-05-12 21:39:45 +0200

    refactor: use UpDownCounter for active tournaments metric
    Refactor TournamentMetrics to use an UpDownCounter for tracking active tournaments instead of an observable gauge. Remove database polling and update the metric in real-time by calling TournamentOpened() and TournamentClosed() when tournaments are created, updated, or deleted. Inject TournamentMetrics into mutation methods to support this change. This improves accuracy and performance of the active tournaments metric.

--- TournamentAPI/Metrics/TournamentMetrics.cs
+++ TournamentAPI/Metrics/TournamentMetrics.cs
@@ -1,40 +1,22 @@
 using System.Diagnostics.Metrics;
-using TournamentAPI.Data;
-using TournamentAPI.Data.Models;
 
 namespace TournamentAPI.Metrics;
 
-public class TournamentMetrics : IHostedService
+public class TournamentMetrics
 {
-    private readonly IServiceScopeFactory _serviceScopeFactory;
     private readonly Counter<int> _tournamentsCreated;
+    private readonly UpDownCounter<int> _activeTournaments;
 
-    public TournamentMetrics(IMeterFactory meterFactory, IServiceScopeFactory serviceScopeFactory)
+    public TournamentMetrics(IMeterFactory meterFactory)
     {
-        _serviceScopeFactory = serviceScopeFactory;
-
         var meter = meterFactory.Create(MetricConstants.TournamentMeterName);
         _tournamentsCreated = meter.CreateCounter<int>("tournamentapi.tournaments.tournaments_created", description: "Number of tournaments created");
-
-        _ = meter.CreateObservableGauge(
-            "tournamentapi.tournaments.active_tournaments",
-            () => GetActiveTournamentsCount(),
-            description: "Number of currently active tournaments");
+        _activeTournaments = meter.CreateUpDownCounter<int>("tournamentapi.tournaments.active_tournaments", description: "Number of currently active tournaments");
     }
 
-    public void IncrementTournamentsCreated()
-    {
-        _tournamentsCreated.Add(1);
-    }
+    public void IncrementTournamentsCreated() => _tournamentsCreated.Add(1);
 
-    private int GetActiveTournamentsCount()
-    {
-        using var scope = _serviceScopeFactory.CreateScope();
-        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        return context.Tournaments.Count(t => t.Status == TournamentStatus.Open);
-    }
+    public void TournamentOpened() => _activeTournaments.Add(1);
 
-    public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    public void TournamentClosed() => _activeTournaments.Add(-1);
 }
