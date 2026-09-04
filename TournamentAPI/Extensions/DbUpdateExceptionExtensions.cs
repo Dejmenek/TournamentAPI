98a9689c2c20336cb9915b63989dbd297910f701
Dejmenek <dejmen14567@gmail.com>
2026-09-04 21:40:37 +0200

    feat: add overload for unique constraint violation check based by message content

--- TournamentAPI/Extensions/DbUpdateExceptionExtensions.cs
+++ TournamentAPI/Extensions/DbUpdateExceptionExtensions.cs
@@ -10,4 +10,11 @@ public static class DbUpdateExceptionExtensions
         return exception.InnerException is SqlException sqlException
             && (sqlException.Number == 2601 || sqlException.Number == 2627);
     }
+
+    public static bool IsUniqueConstraintViolation(this DbUpdateException exception, string constraintOrIndexName)
+    {
+        return exception.InnerException is SqlException sqlException
+            && (sqlException.Number == 2601 || sqlException.Number == 2627)
+            && sqlException.Message.Contains(constraintOrIndexName, StringComparison.OrdinalIgnoreCase);
+    }
 }
