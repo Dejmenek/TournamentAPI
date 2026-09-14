3898083d1844b74d97c23ea02f52a3f24676a1bf
Dejmenek <dejmen14567@gmail.com>
2026-09-14 16:16:07 +0200

    feat: enhance RefreshToken model with new properties
    Replaced `ExpiryDateUtc` with `Expires` for clarity. Added `Created`, `Revoked`, and `ReplacedByToken` properties to track token lifecycle events. Introduced `IsActive` computed property to determine token validity based on revocation status and expiration time.

--- TournamentAPI/Data/Models/RefreshToken.cs
+++ TournamentAPI/Data/Models/RefreshToken.cs
@@ -4,7 +4,11 @@ public class RefreshToken
 {
     public Guid Id { get; set; }
     public string Token { get; set; } = null!;
-    public DateTime ExpiryDateUtc { get; set; }
+    public DateTime Expires { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Revoked { get; set; }
+    public string? ReplacedByToken { get; set; }
     public int UserId { get; set; }
     public ApplicationUser User { get; set; } = null!;
+    public bool IsActive => Revoked is null && DateTime.UtcNow < Expires;
 }
